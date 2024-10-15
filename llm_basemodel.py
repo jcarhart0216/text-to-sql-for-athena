d23fdd744b786cb079d96eb584050c10ccd84313
jcarhart <jcarhart@bamboohealth.com>
2024-10-15 05:23:46 -0400

    Update llm_basemodel.py

--- llm_basemodel.py
+++ llm_basemodel.py
@@ -1,15 +1,13 @@
 from langchain_aws import BedrockLLM
-
 from langchain_community.embeddings import BedrockEmbeddings
 from langchain_community.chat_models import BedrockChat
 from langchain_aws import ChatBedrock
 
+
 class LanguageModel():
-    def __init__(self,client):
+    def __init__(self, client):
         self.bedrock_client = client
-        ############
-        # Anthropic Claude     
-        # Bedrock LLM
+
         inference_modifier = {
                ### "max_tokens_to_sample": 3000,
                 "temperature": 0,
@@ -17,6 +15,7 @@ class LanguageModel():
                 "top_p": 1,
                 "stop_sequences": ["\n\nHuman:"],
             }
+
         print("bedrockllm")
         #self.llm = BedrockLLM(
         #    model_id = "anthropic.claude-v2:1",
@@ -26,14 +25,15 @@ class LanguageModel():
         #                    )
         
         self.llm = ChatBedrock(
-            #model_id = "anthropic.claude-v2:1",
-            model_id = "anthropic.claude-3-sonnet-20240229-v1:0",
+            model_id = "anthropic.claude-v2:1",
+            # model_id = "anthropic.claude-3-sonnet-20240229-v1:0",
                             client = self.bedrock_client, 
                             model_kwargs = inference_modifier 
                             )
         
-        # Embeddings Modules
+        # Embeddings Model
         self.embeddings = BedrockEmbeddings(
             client=self.bedrock_client, 
-            model_id="amazon.titan-embed-text-v1"
+            # model_id="amazon.titan-embed-text-v1"
+            model_id="amazon.titan-embed-text-v2:0"
         )
