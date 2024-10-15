f524654addf62487be10b607b5e0e4c355e0975f
jcarhart <jcarhart@bamboohealth.com>
2024-10-15 05:23:35 -0400

    Update boto_client.py

--- boto_client.py
+++ boto_client.py
@@ -14,27 +14,31 @@ retry_config = Config(
         }
 )
 
+
 class Clientmodules():
     def __init__(self):
         pass
 
     def createBedrockClient():
         session = boto3.session.Session()
-        bedrock_client = session.client('bedrock',config=retry_config)
+        bedrock_client = session.client('bedrock', config=retry_config)
         logger.info(f'bedrock client created for profile')
         return bedrock_client
+
     def createBedrockRuntimeClient():
         session = boto3.session.Session()
-        bedrock_runtime_client = session.client('bedrock-runtime',config=retry_config)
+        bedrock_runtime_client = session.client('bedrock-runtime', config=retry_config)
         logger.info(f'bedrock runtime client created ')
         return bedrock_runtime_client
+
     def createAthenaClient():
         session = boto3.session.Session()
-        athena_client = session.client('athena',config=retry_config)
+        athena_client = session.client('athena', config=retry_config)
         logger.info(f'athena client created ')
         return athena_client
+
     def createS3Client():
         session = boto3.session.Session()
-        s3_client = session.client('s3',config=retry_config)
+        s3_client = session.client('s3', config=retry_config)
         logger.info(f's3 client created !!')
         return s3_client
