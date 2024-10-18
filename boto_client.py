b0d77820e1944875f19b4b63426ab60eb1b8ab35
jcarhart <jcarhart@bamboohealth.com>
2024-10-17 20:33:13 -0400

    Update boto_client.py

--- boto_client.py
+++ boto_client.py
@@ -1,11 +1,15 @@
-import boto3
-from botocore.config import Config
-import logging 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
+import boto3                                    # Imports the AWS SDK for Python, which allows interaction with AWS services
+from botocore.config import Config              # Imports the Config class for setting configurations like retries and timeouts
+import logging                                  # Enables logging of messages for debugging and tracking program execution
 
+# Sets up a logger to record debug-level messages and outputs them to the console
+logger = logging.getLogger(__name__)            # Creates a logger with the name of the current module
+logger.setLevel(logging.DEBUG)                  # Sets the logger to capture all levels of messages at and above DEBUG
+logger.addHandler(logging.StreamHandler())      # Adds a handler to output log messages to the console
+
+
+# Defines a retry configuration for AWS clients to handle transient errors and throttling
 retry_config = Config(
         region_name = 'us-east-1',
         retries = {
@@ -15,22 +19,26 @@ retry_config = Config(
 )
 
 
+# Define a class to encapsulate methods for creating AWS service clients
 class Clientmodules():
     def __init__(self):
         pass
 
+    # Creates a client for AWS Bedrock service
     def createBedrockClient():
-        session = boto3.session.Session()
-        bedrock_client = session.client('bedrock', config=retry_config)
-        logger.info(f'bedrock client created for profile')
-        return bedrock_client
+        session = boto3.session.Session()                                   # Starts a new session with AWS
+        bedrock_client = session.client('bedrock', config=retry_config)     # Creates a Bedrock client with retry config
+        logger.info(f'bedrock client created for profile')                  # Logs the creation of the Bedrock client
+        return bedrock_client                                               # Returns the Bedrock client
 
+    # Creates a client for AWS Bedrock Runtime service
     def createBedrockRuntimeClient():
         session = boto3.session.Session()
         bedrock_runtime_client = session.client('bedrock-runtime', config=retry_config)
         logger.info(f'bedrock runtime client created ')
         return bedrock_runtime_client
 
+    # Creates a client for AWS Athena service
     def createAthenaClient():
         session = boto3.session.Session()
         athena_client = session.client('athena', config=retry_config)
